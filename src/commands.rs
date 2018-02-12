8b236780866d895051b2370a4a44172aedfc6519
Lars Hupel <lars.hupel@mytum.de>
2018-02-12 21:13:32 +0100

    refactoring

--- src/commands.rs
+++ src/commands.rs
@@ -2,12 +2,10 @@ use config::*;
 use dotfiles::*;
 use failure::Error;
 use notify::{DebouncedEvent, RecommendedWatcher, RecursiveMode, Watcher};
-use std::fs::File;
 use std::path::PathBuf;
 use std::sync::mpsc::channel;
 use std::time::Duration;
 use paths::*;
-use util::DotfilesError;
 
 pub use config::init as init;
 
@@ -29,66 +27,11 @@ pub fn watch(config: PathBuf) -> Result<(), Error> {
     }
 }
 
-// TODO implement thorough checking
 pub fn check(config: PathBuf, _thorough: bool, repair: bool) -> Result<(), Error> {
     let config = check_config(&config)?;
     let dotfiles = Dotfiles::load(&config)?;
 
-    info!("Checking for absent content in {:?}", config.contents());
-    let absent_contents = dotfiles.get_absent_files(config.contents().as_path());
-    if absent_contents.is_empty() {
-        info!("No absent content.")
-    }
-    else {
-        if repair {
-            warn!("Cannot fix absent content.");
-        }
-        let msg = format!("Absent content: {:?}", absent_contents);
-        let err = DotfilesError::new(msg);
-        Err(err)?
-    }
-
-    let home = config.get_home()?;
-    info!("Checking for symlinks in {:?}", home);
-    let symlinks = dotfiles.get_symlinks(config.contents().as_path(), home.as_path());
-    for (dotfile, symlink) in &symlinks {
-        match symlink.status {
-            SymlinkStatus::Wrong => {
-                let msg = format!("{:?} is not a symbolic link or symbolic link with wrong target, expected: {:?}", dotfile, symlink.expected);
-                let err = DotfilesError::new(msg);
-                Err(err)?
-            },
-            SymlinkStatus::Absent(ref err) => {
-                let msg = format!("{:?} does not exist, expected symbolic link to {:?} ({:?})", dotfile, symlink.expected, err);
-                Err(DotfilesError::new(msg))?
-            },
-            SymlinkStatus::Ok => ()
-        }
-    }
-    info!("{} symlinks correct.", symlinks.len());
-
+    dotfiles.check(&config, _thorough, repair)?;
     dotfiles.save(&config)?;
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use commands::*;
-    use config::test_util::*;
-    use std::os::unix::fs as unix;
-
-    #[test]
-    fn test_check_success() {
-        let (dir, config) = setup_config();
-        let files = vec![".test1", ".test2"];
-        for f in &files {
-            let path = config.contents().join(f);
-            let msg = format!("{:?} can be created", path);
-            File::create(path).expect(msg.as_ref());
-            unix::symlink(config.contents().join(f), config.get_home().unwrap().join(f)).unwrap();
-        }
-        let dotfiles = Dotfiles::new(Some(files.iter().map(PathBuf::from).collect()));
-        dotfiles.save(&config).unwrap();
-        check(dir.path().join("config.toml"), false, false).unwrap();
-    }
 }
--- src/dotfiles.rs
+++ src/dotfiles.rs
@@ -6,6 +6,7 @@ use std::io::{Read, Write};
 use std::path::{Path, PathBuf};
 use std::vec::Vec;
 use toml;
+use util::DotfilesError;
 
 pub enum SymlinkStatus {
     Ok,
@@ -99,12 +100,52 @@ impl Dotfiles {
             .open(config.dotfiles())?.write(contents.as_bytes())?;
         Ok(())
     }
+
+    // TODO implement thorough checking
+    pub fn check(&self, config: &Config, _thorough: bool, repair: bool) -> Result<(), Error> {
+        info!("Checking for absent content in {:?}", config.contents());
+        let absent_contents = self.get_absent_files(config.contents().as_path());
+        if absent_contents.is_empty() {
+            info!("No absent content.")
+        }
+            else {
+                if repair {
+                    warn!("Cannot fix absent content.");
+                }
+                let msg = format!("Absent content: {:?}", absent_contents);
+                let err = DotfilesError::new(msg);
+                Err(err)?
+            }
+
+        let home = config.get_home()?;
+        info!("Checking for symlinks in {:?}", home);
+        let symlinks = self.get_symlinks(config.contents().as_path(), home.as_path());
+        for (dotfile, symlink) in &symlinks {
+            match symlink.status {
+                SymlinkStatus::Wrong => {
+                    let msg = format!("{:?} is not a symbolic link or symbolic link with wrong target, expected: {:?}", dotfile, symlink.expected);
+                    let err = DotfilesError::new(msg);
+                    Err(err)?
+                },
+                SymlinkStatus::Absent(ref err) => {
+                    let msg = format!("{:?} does not exist, expected symbolic link to {:?} ({:?})", dotfile, symlink.expected, err);
+                    Err(DotfilesError::new(msg))?
+                },
+                SymlinkStatus::Ok => ()
+            }
+        }
+        info!("{} symlinks correct.", symlinks.len());
+
+        Ok(())
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use config::test_util::*;
     use dotfiles::*;
+    use std::fs::File;
+    use std::os::unix::fs as unix;
 
     #[test]
     fn test_empty_dotfiles() {
@@ -113,5 +154,20 @@ mod tests {
         dotfiles.save(&config).unwrap();
         let dotfiles = Dotfiles::load(&config).unwrap();
         assert_eq!(dotfiles, Dotfiles::new(Some(vec![])));
+    }
+
+    #[test]
+    fn test_check_success() {
+        let (_dir, config) = setup_config();
+        let files = vec![".test1", ".test2"];
+        for f in &files {
+            let path = config.contents().join(f);
+            let msg = format!("{:?} can be created", path);
+            File::create(path).expect(msg.as_ref());
+            unix::symlink(config.contents().join(f), config.get_home().unwrap().join(f)).unwrap();
+        }
+        let dotfiles = Dotfiles::new(Some(files.iter().map(PathBuf::from).collect()));
+        dotfiles.save(&config).unwrap();
+        dotfiles.check(&config,false, false).unwrap();
     }
 }
